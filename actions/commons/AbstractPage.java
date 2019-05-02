2c17e42662c6d253d0858d71da786dbfb5ac6528
NguyenTrongNghia <NghiaTN23@gmail.com>
2019-05-02 21:49:16 +0700

    upload topic 19

--- actions/commons/AbstractPage.java
+++ actions/commons/AbstractPage.java
@@ -1,5 +1,117 @@
 package commons;
 
-public class AbstractPage {
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//HAM WEB DROW
+public class AbstractPage {
+	WebElement element;
+	JavascriptExecutor javascripExecutor;
+	WebDriverWait waiExplicit;
+	
+	public void openAnyUrl(WebDriver driver, String Url)
+	{
+		driver.get(Url);
+	}
+	public String getCurrentPageUrl(WebDriver driver)
+	{
+		return driver.getCurrentUrl();
+	}
+	public String getPageTitle(WebDriver driver)
+	{
+		return driver.getTitle();
+	}
+	public String getPageSoureCode(WebDriver driver)
+	{
+		return driver.getPageSource();
+	}
+	public void backtoPreviousPage(WebDriver driver)
+	{
+		driver.navigate().back();
+	}
+	public void refreshCurentPage(WebDriver driver)
+	{
+		driver.navigate().refresh();
+	}
+	public void forwardToNextPage(WebDriver driver)
+	{
+		driver.navigate().forward();
+	}
+	public void AccepAlert(WebDriver driver)
+	{
+		driver.switchTo().alert().accept();
+	}
+	public void CancelAlert(WebDriver driver)
+	{
+		driver.switchTo().alert().dismiss();
+	}
+	public void GettextAlert(WebDriver driver)
+	{
+		driver.switchTo().alert().getText();
+	}
+	
+	//WEB ELEMENT
+	public void clickToElement(WebDriver driver, String locator)
+	{
+		 element = driver.findElement(By.xpath(locator));
+		element.click();
+	}
+	public void sendKey(WebDriver driver, String value, String locator)
+	{
+		 element = driver.findElement(By.xpath(locator));
+		element.sendKeys(value);
+	}
+	public void selectItemInDropdown(WebDriver driver, String locator, String value)
+	{
+		element = driver.findElement(By.xpath(locator));
+		Select select = new Select(element);
+		select.selectByVisibleText(value);		
+	}
+	public String GetselectItemInDropdown(WebDriver driver, String locator, String value)
+	{
+		element = driver.findElement(By.xpath(locator));
+		Select select = new Select(element);
+		return select.getFirstSelectedOption().getText();	
+	}
+	public void SelectItemInCustomDropdown(WebDriver driver, String parentXpath, String alltemXpath, String expectedValueItem ) throws InterruptedException
+	{
+		WebElement parentDropdown =driver.findElement(By.xpath(parentXpath));
+		waiExplicit = new WebDriverWait(driver, 30);
+		javascripExecutor.executeScript("arguments[0].click()",parentDropdown );
+		/* cho tat ca cac gia tri trong dropdow duo load thanh cong*/
+		waiExplicit.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(alltemXpath)));
+		List<WebElement> allItems = driver.findElements(By.xpath(alltemXpath));
+		System.out.println("Tat ca phan tu trong Dropdown =" + allItems.size());
+		// duyet qua tat ca cac phan tu cho den khi thoa man  dieu kien
+		for(WebElement childElement : allItems)
+		{
+			System.out.println("Text oi lan get =" +childElement.getText());
+			if(childElement.getText().contains(expectedValueItem))
+			{
+				javascripExecutor.executeScript("arguments[0].scrollIntoView(true);",childElement );
+				Thread.sleep(1000);
+				//click vao item can chon
+				if(childElement.isDisplayed())
+				{
+					//Selenium click
+					childElement.click();
+				}
+				else
+				{
+					//JS click
+					javascripExecutor.executeScript("arguments[0].click();",childElement );
+				}
+				Thread.sleep(1000);
+				break;
+			}
+			
+		}
+		
+	}
 }
--- testcases/com/bankguru/account/Account_Level01_StepByStep_TopDown.java
+++ testcases/com/bankguru/account/Account_Level01_StepByStep_TopDown.java
@@ -13,72 +13,72 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 
 public class Account_Level01_StepByStep_TopDown {
- WebDriver driver;
- String loginPageUrl, userIdInfor, passwordInfor, email;
- 
-  @BeforeClass
-  public void beforeClass() {
-	  driver = new FirefoxDriver();
-	  driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	  driver.manage().window().maximize();
-	  driver.get("http://demo.guru99.com/v4/");
-	  
-	  email ="selenium09" + ramdomNumber() +"@gmail.com";
-  }
-  
-  @Test
-  public void TC_01_RegisterToSytem() {
-	  
-	  // step 01 - check Login page displayed
-	  Assert.assertTrue(driver.findElement(By.xpath("//form[@name ='frmLogin']")).isDisplayed());
-	 
-	  //Step 02 - click to "here" link 	 -> open Register Page
-	  loginPageUrl = driver.getCurrentUrl();
-	  driver.findElement(By.xpath("//a[text()='here']")).click();
-	 
-	  //step 03 - check register page display
-	  Assert.assertTrue(driver.findElement(By.xpath("//input[@name='emailid']")).isDisplayed());
-	 
-	  //Step 04 - Input random email to EMAIL textbox
-	  driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
-	 
-	  // Step 05 - Click to submit button
-	  driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-	  
-	  // Step 06 Get user/password information
-	  userIdInfor = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
-	 passwordInfor = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
-	  System.out.println("User:" +userIdInfor+ " - password: " + passwordInfor);
-  }
-  
-  @Test
-  public void TC_02_LoginToSytem() {
-	  //step 01 open login page
-	  driver.get(loginPageUrl);
-	  Assert.assertTrue(driver.findElement(By.xpath("//form[@name ='frmLogin']")).isDisplayed());
-	  //step 02 Input valid data to user and password textbox
-	  driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userIdInfor);
-	  driver.findElement(By.xpath("//input[@name='password']")).sendKeys(passwordInfor);
-	  
-	  //Step 03 Click to login button
-	  driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-	  
-	  //Step 04 Check Home page Displayed
-	  Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
-      
-	  //Step 05 - check UserID infor displayed in Home page 
-	  Assert.assertTrue(driver.findElement(By.xpath("//td[text()='Manger Id : " + userIdInfor + "']")).isDisplayed());
-  }
+	WebDriver driver;
+	String loginPageUrl, userIdInfor, passwordInfor, email;
 
-  @AfterClass
-  public void afterClass() {
-	  driver.quit();
-  }
-   
-  public int ramdomNumber()
-  {
-	  Random random = new Random();
-	  return random.nextInt(999999);
-	  
-  }
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4/");
+
+		email = "selenium09" + ramdomNumber() + "@gmail.com";
+	}
+
+	@Test
+	public void TC_01_RegisterToSytem() {
+
+		// step 01 - check Login page displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name ='frmLogin']")).isDisplayed());
+
+		// Step 02 - click to "here" link -> open Register Page
+		loginPageUrl = driver.getCurrentUrl();
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+
+		// step 03 - check register page display
+		Assert.assertTrue(driver.findElement(By.xpath("//input[@name='emailid']")).isDisplayed());
+
+		// Step 04 - Input random email to EMAIL textbox
+		driver.findElement(By.xpath("//input[@name='emailid']")).sendKeys(email);
+
+		// Step 05 - Click to submit button
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+
+		// Step 06 Get user/password information
+		userIdInfor = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		passwordInfor = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+		System.out.println("User:" + userIdInfor + " - password: " + passwordInfor);
+	}
+
+	@Test
+	public void TC_02_LoginToSytem() {
+		// step 01 open login page
+		driver.get(loginPageUrl);
+		Assert.assertTrue(driver.findElement(By.xpath("//form[@name ='frmLogin']")).isDisplayed());
+		// step 02 Input valid data to user and password textbox
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userIdInfor);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(passwordInfor);
+
+		// Step 03 Click to login button
+		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
+
+		// Step 04 Check Home page Displayed
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]"))
+				.isDisplayed());
+
+		// Step 05 - check UserID infor displayed in Home page
+		Assert.assertTrue(driver.findElement(By.xpath("//td[text()='Manger Id : " + userIdInfor + "']")).isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int ramdomNumber() {
+		Random random = new Random();
+		return random.nextInt(999999);
+
+	}
 }
